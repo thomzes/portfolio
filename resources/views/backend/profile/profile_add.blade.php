3597496738676265d832d94b4f86374bb4f2d09d
Thomzes <thomz161@gmail.com>
2022-05-23 17:06:55 +0700

    install intervention image

--- resources/views/backend/profile/profile_add.blade.php
+++ resources/views/backend/profile/profile_add.blade.php
@@ -12,24 +12,26 @@
 
         <form>
             <div class="form-group">
-              <label for="exampleInputEmail1">Name</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <label for="name">Name</label><span class="text-danger">*</span>
+              <input type="text" class="form-control" id="name" name="name" placeholder="Enter Full Name..." required>
             </div>
             <div class="form-group">
-              <label for="exampleInputEmail1">Status</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <label for="status">Status</label>
+              <input type="text" class="form-control" id="status" name="status" placeholder="Enter Status...">
+              <small id="emailHelp" class="form-text text-muted">If You're not input the status, status default is Student </small>
+
             </div>
             <div class="form-group">
-              <label for="exampleInputEmail1">Job</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <label for="job">Job</label><span class="text-danger">*</span>
+              <input type="text" class="form-control" id="job" placeholder="Enter Job..." name="job">
             </div>
             <div class="form-group">
-                <label for="exampleFormControlTextarea1">Description</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <label for="description">Description</label><span class="text-danger">*</span>
+                <textarea class="form-control" id="description" rows="3" name="description" placeholder="Enter Description..." required></textarea>
               </div>
             <div class="form-group">
-              <label for="exampleInputEmail1">Image</label>
-              <input type="file" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <label for="image">Image</label><span class="text-danger">*</span>
+              <input type="file" class="form-control" id="image" name="image" placeholder="" required>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
           </form>
